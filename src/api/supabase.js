f9a0e63a65e5567108e441f5698aa779fe1330e5
Labros <labros.lazarakis@gmail.com>
2026-03-07 13:03:25 +0200

    Secure all API keys in .env

--- src/api/supabase.js
+++ src/api/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fqvjtrhplxceztnrozhr.supabase.co';
-const supabaseKey = 'sb_publishable_EkHVAjuS-cr70Zt_dRSL3Q_VJfGJvRJ';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
